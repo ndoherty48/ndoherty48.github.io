df4bd0c987e4cb4568ed6a295db1a1bebf3843ed
Nathan Doherty <nathandohertyapplications@outlook.com>
2025-12-20 18:58:31 +0000

    Aspirify solution using a file-based apphost

---
+++ apphost.cs
@@ -0,0 +1,11 @@
+﻿#:package Aspire.Hosting.JavaScript@13.2.0-preview.1.25619.3
+#:sdk Aspire.AppHost.Sdk@13.2.0-preview.1.25619.3
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var fe = builder.AddJavaScriptApp("frontend", "./frontend")
+    .WithRunScript("dev")
+    .WithBuildScript("build")
+    .WithHttpEndpoint(targetPort: 4321, isProxied: false);
+
+builder.Build().Run();
