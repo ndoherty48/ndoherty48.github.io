3df2db4a4f94bb9e9bc99a201f55dad263923ae5
Nathan Doherty <nathandohertyapplications@outlook.com>
2025-12-20 18:57:45 +0000

    Add frontend using astro portfolio template, still a work in progress

---
+++ frontend/src/content.config.ts
@@ -0,0 +1,43 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+export const collections = {
+	projects: defineCollection({
+		// Load Markdown files in the src/content/work directory.
+		loader: glob({ base: './src/content/projects', pattern: '**/*.md' }),
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
+		}),
+	}),
+	blog: defineCollection({
+		// Load Markdown files in the src/content/work directory.
+		loader: glob({ base: './src/content/blog', pattern: '**/*.md' }),
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
+		}),
+	}),
+	employment: defineCollection({
+		// Load Markdown files in the src/content/work directory.
+		loader: glob({ base: './src/content/employment', pattern: '**/*.md' }),
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			employer: z.string(),
+			startDate: z.date(),
+			endDate: z.date().optional(),
+			tags: z.array(z.string()).optional(),
+			img: z.string(),
+			img_alt: z.string().optional(),
+		}),
+	}),
+};
